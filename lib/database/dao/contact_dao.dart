5f10137a172e85eeefc5c6541f94924a19dc4ea3
MARCOSM-MARTINS <marcosm.caixa@gmail.com>
2020-04-13 14:51:03 -0300

    implementação do delete

--- lib/database/dao/contact_dao.dart
+++ lib/database/dao/contact_dao.dart
@@ -4,63 +4,60 @@ import 'package:sqflite/sqflite.dart';
 import '../app_database.dart';
 
 class ContactDao {
-
   static const String tableSql = 'CREATE TABLE $_tableName('
-          '$_id INTEGER PRIMARY KEY, '
-          '$_name TEXT, '
-          '$_accountNumber INTEGER)';
+      '$_id INTEGER PRIMARY KEY, '
+      '$_name TEXT, '
+      '$_accountNumber INTEGER)';
 
-  static const String  _tableName = 'contacts';
+  static const String _tableName = 'contacts';
   static const String _id = 'id';
   static const String _name = 'name';
   static const String _accountNumber = 'account_number';
 
   Future<int> save(Contact contact) async {
-  final Database db = await getDatabase();
-  Map<String, dynamic> contactMap = _toMap(contact);
-     return db.insert(_tableName, contactMap);
-  
-  // return getDatabase().then((db) {
-  //   final Map<String, dynamic> contactMap = Map();
-  //   contactMap['name'] = contact.name;
-  //   contactMap['account_number'] = contact.accountNumber;
-  //   return db.insert('contacts', contactMap);
-  // });
-}
+    final Database db = await getDatabase();
+    Map<String, dynamic> contactMap = _toMap(contact);
+    return db.insert(_tableName, contactMap);
+
+    // return getDatabase().then((db) {
+    //   final Map<String, dynamic> contactMap = Map();
+    //   contactMap['name'] = contact.name;
+    //   contactMap['account_number'] = contact.accountNumber;
+    //   return db.insert('contacts', contactMap);
+    // });
+  }
 
   Map<String, dynamic> _toMap(Contact contact) {
     final Map<String, dynamic> contactMap = Map();
-       contactMap[_name] = contact.name;
-       contactMap[_accountNumber] = contact.accountNumber;
+    contactMap[_name] = contact.name;
+    contactMap[_accountNumber] = contact.accountNumber;
     return contactMap;
   }
 
-Future<List<Contact>> findAll() async {
-
-  final Database db = await getDatabase();
-  final List<Map<String, dynamic>> result =
-    await db.query(_tableName);
+  Future<List<Contact>> findAll() async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tableName);
     List<Contact> contacts = _toList(result);
-      return contacts;
-      
-  // return getDatabase().then((db) {
-  //   return db.query('contacts').then((maps) {
-  //     final List<Contact> contacts = List();
-  //     for (Map<String, dynamic> map in maps) {
-  //       final Contact contact = Contact(
-  //         map['id'],
-  //         map['name'],
-  //         map['account_number'],
-  //       );
-  //       contacts.add(contact);
-  //     }
-  //     return contacts;
-  //   });
-  // });
-}
+    return contacts;
 
-List<Contact> _toList(List<Map<String, dynamic>> result) {
-  final List<Contact> contacts = List();
+    // return getDatabase().then((db) {
+    //   return db.query('contacts').then((maps) {
+    //     final List<Contact> contacts = List();
+    //     for (Map<String, dynamic> map in maps) {
+    //       final Contact contact = Contact(
+    //         map['id'],
+    //         map['name'],
+    //         map['account_number'],
+    //       );
+    //       contacts.add(contact);
+    //     }
+    //     return contacts;
+    //   });
+    // });
+  }
+
+  List<Contact> _toList(List<Map<String, dynamic>> result) {
+    final List<Contact> contacts = List();
     for (Map<String, dynamic> row in result) {
       final Contact contact = Contact(
         row[_id],
@@ -69,7 +66,17 @@ List<Contact> _toList(List<Map<String, dynamic>> result) {
       );
       contacts.add(contact);
     }
-  return contacts;
-}
+    return contacts;
+  }
 
+  Future<void> update(Contact contact) async {
+    final Database db = await getDatabase();
+
+    await db.update(
+      _tableName,
+      _toMap(contact),
+      where: '$_id = ?',
+      whereArgs: [contact.id]
+    );
+  }
 }
